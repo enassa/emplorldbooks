ec1c1a438e8dbb87937eacfc02a3941d751d8525
enassan <assanenathaniel@gmai.com>
2019-03-13 05:05:42 +0000

    s

--- dbdetails.php
+++ dbdetails.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $tablename = "books";
 $dbname = "heroku_64357b7c6a8e2fc";
 $servername = "us-cdbr-iron-east-03.cleardb.net";   
@@ -7,6 +7,18 @@ $username="bca0d264f64203";
 $password="f4f0322a";
 $counter = 1;
 $bookname = 0;
+*/
+
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername   = $cleardb_url["host"];
+$username = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$dbname       = substr($cleardb_url["path"],1);
+$counter = 1;
+$bookname = 0;
+
+
+
 /*
 $tablename = "books";
 $dbname = "bookinfo";
